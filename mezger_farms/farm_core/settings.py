60c18ee2c31e5ffe8ccfb72cf430c72a396ad0f4
mezgerj <mezger.10@gmail.com>
2018-05-28 23:01:13 -0400

    reworked project structure

--- mezger_farms/farm_core/settings.py
+++ mezger_farms/farm_core/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = '4-w1#h&3q1nkgvrv2e8r53*_mrv^l@@b6cf9*+4qqd9xazg!f9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'farm',
 ]
 
 MIDDLEWARE = [
@@ -67,8 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mezger_farms.wsgi.application'
-
+WSGI_APPLICATION = 'farm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
@@ -79,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -99,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -112,7 +108,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
--- mezger_farms/farm_core/wsgi.py
+++ mezger_farms/farm_core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mezger_farms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "farm.settings")
 
 application = get_wsgi_application()
