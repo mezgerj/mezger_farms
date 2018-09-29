46af13656db6eb74ff0e88f4de314933b3008323
mezgerj <mezger.10@gmail.com>
2018-09-28 23:11:48 -0400

    updated test fixture

--- mezger_farms/farm_core/tests/test_model.py
+++ mezger_farms/farm_core/tests/test_model.py
@@ -4,8 +4,10 @@ from farm_core.models import Farm
 
 
 class TestExample(TestCase):
-    fixtures = ['fixture.json']
+    fixtures = ['mezger_farms/farm_core/fixtures/fixture.json']
 
     def test_farm_repr(self):
+        import os
+        os.listdir()
         farm = Farm.objects.first()
         self.assertEquals(farm.__repr__(), 'Frank Mezger Home')
